844453ef5740025d424df9638b52162d2451d5e0
bbtechpro <billy.barber@gmail.com>
2026-05-25 21:16:44 -0400

    Added  scope: ['user:email']  under the GitHubStrategy.

--- utils/passportGhub.js
+++ utils/passportGhub.js
@@ -7,7 +7,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.GITHUB_CALLBACK_URL, // e.g., 'http://localhost:3001/api/users/auth/github/callback'
+      callbackURL: process.env.GITHUB_CALLBACK_URL, // e.g., 'http://localhost:3001/api/users/auth/github/callback',
+      scope: ['user:email'] 
     },
     // This is the "verify" callback
     async (accessToken, refreshToken, profile, done) => {
